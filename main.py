fa7f694a2f1f58ce32d3da165554f526cb9ab16f
Simcso <118608686+Simcso@users.noreply.github.com>
2023-02-05 17:41:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
 import sys
 import pygame
-from load_map import Map, view_map
+from load_map import Map, view_map, find_toponym, TextInput
 
 
+pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode()
-map = Map((47.8908, 56.6388))
+
+cords = find_toponym('Йошкар-Ола')
+map = Map(cords, cords)
 view_map(screen)
+
+text = TextInput(screen, 20, 20, 300, 30, 'Введите запрос', 20)
 while True:
+    text.update()
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN and event.scancode in [78, 75]:
-            map.change_delta('+' if event.scancode == 78 else '-')
-            view_map(screen)
-        elif event.type == pygame.KEYDOWN and event.scancode in [79, 80, 81, 82]:
-            map.change_cords((0 if event.scancode not in [79, 80] else {79: '+', 80: '-'}[event.scancode],
-                              0 if event.scancode not in [81, 82] else {82: '+', 81: '-'}[event.scancode]))
-            view_map(screen)
-        elif event.type == pygame.KEYDOWN and event.scancode == 41:
-            # Завершаем программу
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN and event.scancode in [30, 31, 32]:
-            map.change_type(event.scancode)
-            view_map(screen)
+        if text.text_input:
+            if event.type == pygame.TEXTINPUT:
+                text.text += event.text
+            elif event.type == pygame.KEYDOWN and event.scancode == 42:
+                text.text = text.text[:-1]
+            elif event.type == pygame.KEYDOWN and event.scancode == 40:
+                text.text_input = False
+                cords = find_toponym(text.get_text())
+                if cords:
+                    map = Map(cords, cords, map.delta, map.type, map.name)
+                    view_map(screen)
+        else:
+            if event.type == pygame.KEYDOWN and event.scancode in [78, 75]:
+                map.change_delta('+' if event.scancode == 78 else '-')
+                view_map(screen)
+            elif event.type == pygame.KEYDOWN and event.scancode in [79, 80, 81, 82]:
+                map.change_cords((0 if event.scancode not in [79, 80] else {79: '+', 80: '-'}[event.scancode],
+                                  0 if event.scancode not in [81, 82] else {82: '+', 81: '-'}[event.scancode]))
+                view_map(screen)
+            elif event.type == pygame.KEYDOWN and event.scancode == 41:
+                # Завершаем программу
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN and event.scancode in [30, 31, 32]:
+                map.change_type(event.scancode)
+                view_map(screen)
     pygame.display.flip()
     clock.tick(60)
 
